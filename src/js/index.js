460f30e14bcbd7b7c513483fcfde72eb05634c22
Tomek Wozniak <tomekwozniakk@gmail.com>
2020-06-07 23:18:12 +0200

    added date function, and new content

--- src/js/index.js
+++ src/js/index.js
@@ -32,33 +32,33 @@ const age = "34";
 // createContent('.day-summary__title--js', '12.05.2020 - Siema, tutaj podmieniony tytuł!')
 
 
-function greetOne(age, myName) {
-    console.log(`Witaj! Nazywam się ${myName} i mam ${age} lata.`)
-}
+// function greetOne(age, myName) {
+//     console.log(`Witaj! Nazywam się ${myName} i mam ${age} lata.`)
+// }
 
-greetOne(age, myName);
-greetOne(2, 'Janusz');
+// greetOne(age, myName);
+// greetOne(2, 'Janusz');
 
-const greetTwo = (age, myName) => {
-    console.log(`Witaj przybyszu! Nazywam się ${myName} i mam ${age} lata. To jest arrowfunction`)
-}
+// const greetTwo = (age, myName) => {
+//     console.log(`Witaj przybyszu! Nazywam się ${myName} i mam ${age} lata. To jest arrowfunction`)
+// }
 
-greetTwo(age, myName);
-greetTwo(3, 'Mariola');
+// greetTwo(age, myName);
+// greetTwo(3, 'Mariola');
 
-const bottle = {
-    volume: 700,
-    pour: (vessel) => {
-        console.log(`You pour mysterious liquid from the bottle. Your ${vessel} is full.`)
-    },
-    isNotEmpty: true,
-    label: 'Wyborowa',
-    percentage: 40,
-}
+// const bottle = {
+//     volume: 700,
+//     pour: (vessel) => {
+//         console.log(`You pour mysterious liquid from the bottle. Your ${vessel} is full.`)
+//     },
+//     isNotEmpty: true,
+//     label: 'Wyborowa',
+//     percentage: 40,
+// }
 
-console.log(bottle);
-bottle.pour('glass');
-bottle.pour('bucket(!)');
+// console.log(bottle);
+// bottle.pour('glass');
+// bottle.pour('bucket(!)');
 
 
 const hamburger = document.querySelector('.hamburger--js');
@@ -67,3 +67,14 @@ hamburger.addEventListener('click', () => {
     const nav = document.querySelector('.navigation--js');
     nav.classList.toggle('navigation--open');
 });
+
+
+// adding moment
+
+import moment from 'moment';
+
+function addDate(querySelectorContent, date){
+const element = document.querySelector(querySelectorContent);
+element.innerHTML = (`Witaj w moim dzienniku, dzisiaj jest ` + date);}
+
+addDate('.date--js', moment().locale('pl').format('LL'));
